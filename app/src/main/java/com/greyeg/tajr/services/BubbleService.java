d2a4fd1d6b21090d734395f416330b7e18e62bb0
ahmdkhled <ahmdkhled@gmail.com>
2019-09-06 18:52:20 +0200

    feat:show tajr bubble on screen

---
+++ app/src/main/java/com/greyeg/tajr/services/BubbleService.java
@@ -0,0 +1,64 @@
+package com.greyeg.tajr.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import androidx.annotation.Nullable;
+import com.greyeg.tajr.R;
+
+public class BubbleService extends Service {
+
+    private WindowManager mWindowManager;
+    private View bubbleView;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        bubbleView = LayoutInflater.from(this).inflate(R.layout.bubble, null);
+
+        WindowManager.LayoutParams params;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                    PixelFormat.TRANSLUCENT);
+        }else{
+            params = new WindowManager.LayoutParams(
+                    WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.TYPE_PHONE,
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                    PixelFormat.TRANSLUCENT);
+        }
+
+        params.gravity = Gravity.TOP | Gravity.START;
+        params.x = 0;
+        params.y = 100;
+
+        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        mWindowManager.addView(bubbleView, params);
+
+
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (bubbleView != null) mWindowManager.removeView(bubbleView);
+    }
+}
