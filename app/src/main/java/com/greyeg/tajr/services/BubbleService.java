5841bcf40bb08d62825d9e8de30bd4fdca82e35e
ahmdkhled <ahmdkhled@gmail.com>
2019-09-08 02:42:08 +0200

    make flasher for detecting getting user id

--- app/src/main/java/com/greyeg/tajr/services/BubbleService.java
+++ app/src/main/java/com/greyeg/tajr/services/BubbleService.java
@@ -3,6 +3,7 @@ package com.greyeg.tajr.services;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.PixelFormat;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 import android.view.Gravity;
@@ -43,6 +44,8 @@ public class BubbleService extends Service {
     private float startY;
     public static boolean isRunning=false;
     private   boolean deleteViewAdded=false;
+    private Handler handler;
+    private Runnable runnable;
 
     @Nullable
     @Override
@@ -107,7 +110,6 @@ public class BubbleService extends Service {
                     }
                 });
 
-        getUserId("Ahmed Khaled");
 
 
     }
@@ -138,7 +140,7 @@ public class BubbleService extends Service {
                     float endX = event.getX();
                     float endY = event.getY();
                     if (isAClick(startX, endX, startY, endY)) {
-                        if (userName==null||userID==null){
+                        if (userName==null){
                             bubbleView.findViewById(R.id.gotUserName)
                                     .setBackgroundResource(R.drawable.circle_gray);
                             return true;
@@ -146,11 +148,17 @@ public class BubbleService extends Service {
                         else{
                             bubbleView.findViewById(R.id.gotUserName)
                                     .setBackgroundResource(R.drawable.circle_green);
+                            if (userID==null){
+                                getUserId("Ahmed Khaled");
+                            }else {
+                                if (expandedView.getVisibility()==View.GONE)
+                                    expandedView.setVisibility(View.VISIBLE);
+                                else
+                                    expandedView.setVisibility(View.GONE);
+                            }
+
                         }
-                        if (expandedView.getVisibility()==View.GONE)
-                        expandedView.setVisibility(View.VISIBLE);
-                            else
-                                expandedView.setVisibility(View.GONE);
+
                     }
 
 
@@ -243,11 +251,13 @@ public class BubbleService extends Service {
     private void getUserId(String userName){
         String token= SharedHelper.getKey(getApplicationContext(), LoginActivity.TOKEN);
         Log.d("SUBSCRIPERR", "token: "+token);
+        startFlasher();
         BaseClient.getService()
                 .getSubscriberInfo(token,userName)
                 .enqueue(new Callback<SubscriberInfo>() {
                     @Override
                     public void onResponse(Call<SubscriberInfo> call, Response<SubscriberInfo> response) {
+                        stopFlasher();
                         SubscriberInfo subscriberInfo=response.body();
                         if (response.isSuccessful()&&subscriberInfo!=null){
                             Log.d("SUBSCRIPERR","subscriper "+response.body().getData());
@@ -257,7 +267,7 @@ public class BubbleService extends Service {
                                         "there is a problem fetching user data"
                                         , Toast.LENGTH_SHORT).show();
                             }else if (subscribersData.size()==1){
-
+                                userID=subscribersData.get(0).getPsid();
                             }else {
 
 
@@ -276,9 +286,43 @@ public class BubbleService extends Service {
 
                     @Override
                     public void onFailure(Call<SubscriberInfo> call, Throwable t) {
+                        stopFlasher();
                         Log.d("SUBSCRIPERR", "onFailure: "+t.getMessage());
                     }
                 });
     }
 
-}
+    boolean run=false;
+    private void startFlasher(){
+
+        handler=new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("SUBSCRIPERR","run ");
+                if (run)
+                    bubbleView.findViewById(R.id.gotUserId)
+                            .setBackgroundResource(R.drawable.circle_green);
+                else
+                    bubbleView.findViewById(R.id.gotUserId)
+                            .setBackgroundResource(R.drawable.circle_gray);
+
+                handler.postDelayed(this,300);
+                run=!run;
+            }
+        });
+
+//        bubbleView.findViewById(R.id.gotUserId)
+//                .setBackgroundResource(R.drawable.circle_green);
+    }
+
+    private void stopFlasher(){
+        handler.removeCallbacksAndMessages(null);
+        //handler.removeCallbacks(runnable);
+        bubbleView.findViewById(R.id.gotUserId)
+                .setBackgroundResource(R.drawable.circle_green);
+    }
+
+
+
+    }
